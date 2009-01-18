3c4b38b766faa7f5eec342efaaf5675d1fe3d0d7
Nolan Eakins <sneakin@semanticgap.com>
2009-01-18 16:35:04 -0500

    Removed the logging, fixed how the args are passed, and parenthesised some args.

--- lib/mr_wizard/routing.rb
+++ lib/mr_wizard/routing.rb
@@ -1,13 +1,21 @@
 module MrWizard
   module Routing
-    def wizard(map, name, path_template = nil, options = Hash.new)
-      path_template ||= "/#{name}"
-      controller = options[:controller]
-      RAILS_DEFAULT_LOGGER.debug("Routing wizard #{name}, #{path_template}, #{controller}")
+    def wizard(map, name, *args)
+      options = args.pop if args.last.kind_of? Hash
+      options ||= Hash.new
 
-      map.with_options :controller => controller, :defaults => { :step => nil } do |w|
-        w.named_route(name, "#{path_template}/:step", :action => 'show', :conditions => { :method => :get })
-        w.named_route("#{name}_form", "#{path_template}/:step", :action => 'create', :conditions => { :method => :post })
+      path_template = args.pop
+      path_template ||= "/#{name}"
+
+      controller = options[:controller]
+
+      map.with_options(:controller => controller, :defaults => { :step => nil }) do |w|
+        w.named_route(name.to_s, "#{path_template}/:step",
+                      :action => 'show',
+                      :conditions => { :method => :get })
+        w.named_route("#{name}_form", "#{path_template}/:step",
+                      :action => 'create',
+                      :conditions => { :method => :post })
       end
     end
   end
